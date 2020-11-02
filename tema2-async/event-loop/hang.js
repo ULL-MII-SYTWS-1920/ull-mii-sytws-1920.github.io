240d731838da77a1a89c574dea4b10455a09b083
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-11-02 14:38:17 +0000

    2020-2021

---
+++ tema2-async/event-loop/hang.js
@@ -0,0 +1,15 @@
+let i = 0;
+
+let start = Date.now();
+
+function count() {
+
+  // do a heavy job
+  for (let j = 0; j < 1e9; j++) {
+    i++;
+  }
+
+  alert("Done in " + (Date.now() - start) + 'ms');
+}
+
+count();
